3dd3e4661105b687b3ff932ee84df5d7d6c067ee
Sam Noh <samnoh21@outlook.com>
2020-05-27 15:19:47 +1200

    update checkUpdate

--- src/helpers/checkUpdate.js
+++ src/helpers/checkUpdate.js
@@ -4,14 +4,13 @@ const pkg = require('../../package.json');
 
 const notifier = updateNotifier({
     pkg,
-    updateCheckInterval: 1000 * 60 * 60,
+    updateCheckInterval: 1,
     shouldNotifyInNpmScript: true,
 });
 
 module.exports = () => {
     if (notifier.update) {
         notifier.notify({
-            message: 'Run `{updateCommand}` to update.',
             isGlobal: true,
             defer: false,
         });
