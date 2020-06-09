683ec94fe2beee1397e62cfdbf29493971b66dcb
Sam Noh <samnoh21@outlook.com>
2020-06-09 14:48:20 +1200

    .

--- src/crawler/ppomppu/index.js
+++ src/crawler/ppomppu/index.js
@@ -28,118 +28,123 @@ class Ppomppu extends CommunityCrawler {
     async getPosts() {
         await this.page.goto(getUrl(this.currentBoard.value, this.pageNumber, this.sortUrl.value));
 
-        const posts = await this.page.evaluate((baseUrl) => {
-            const lists = document.querySelectorAll('.bbsList li a');
+        const posts = await this.page.evaluate(
+            (baseUrl, currentBoard) => {
+                const lists = document.querySelectorAll('.bbsList li a');
 
-            if (lists.length) {
-                return Array.from(lists)
-                    .map((list) => {
-                        const author = list.querySelector('.ct') || list.querySelector('.name');
-                        const numberOfComments = list.querySelector('.rp');
+                if (lists.length) {
+                    return Array.from(lists)
+                        .map((list) => {
+                            const author = list.querySelector('.ct') || list.querySelector('.name');
+                            const numberOfComments = list.querySelector('.rp');
 
-                        const title = list.querySelector('strong') || list.querySelector('.title');
+                            const title =
+                                list.querySelector('strong') || list.querySelector('.title');
 
-                        try {
-                            numberOfComments && title.removeChild(numberOfComments);
-                        } catch (e) {}
+                            try {
+                                numberOfComments && title.removeChild(numberOfComments);
+                            } catch (e) {}
 
-                        let time = list.querySelector('.b');
-                        let category, hit, upVotes, downVotes;
+                            let time = list.querySelector('.b');
+                            let category, hit, upVotes, downVotes;
 
-                        if (time) {
-                            const infoEl = list
-                                .querySelector('.hi')
-                                .innerText.split('/')
-                                .map((el) => el.replace(/[^0-9]/g, ''));
-                            hit = infoEl[0];
-                            upVotes = infoEl[1];
-                            downVotes = infoEl[2];
-                        } else {
-                            const timeEl = list.querySelector('.time');
+                            if (time) {
+                                const infoEl = list
+                                    .querySelector('.hi')
+                                    .innerText.split('/')
+                                    .map((el) => el.replace(/[^0-9]/g, ''));
+                                hit = infoEl[0];
+                                upVotes = infoEl[1];
+                                downVotes = infoEl[2];
+                            } else {
+                                const timeEl = list.querySelector('.time');
 
-                            const infoEl = timeEl.innerText
-                                .replace(/[^0-9:\-]/g, '/')
-                                .split('/')
-                                .filter((a) => a);
+                                const infoEl = timeEl.innerText
+                                    .replace(/[^0-9:\-]/g, '/')
+                                    .split('/')
+                                    .filter((a) => a);
 
-                            timeEl.removeChild(timeEl.querySelector('time'));
-                            category = timeEl.innerText.replace('|', '').trim();
-                            time = infoEl[0];
-                            hit = infoEl[1];
-                            upVotes = infoEl[2];
-                            downVotes = infoEl[3];
-                        }
+                                timeEl.removeChild(timeEl.querySelector('time'));
+                                category = timeEl.innerText.replace('|', '').trim();
+                                time = infoEl[0];
+                                hit = infoEl[1];
+                                upVotes = infoEl[2];
+                                downVotes = infoEl[3];
+                            }
+
+                            let link = list.getAttribute('href');
+
+                            if (link && !link.includes('/new/')) {
+                                if (currentBoard === 'ppomppu2') {
+                                    link = '/new/bbs_view.php' + link;
+                                } else {
+                                    link = '/new/' + link;
+                                }
+                            }
+
+                            return (
+                                author &&
+                                author.innerText && {
+                                    category,
+                                    title: title.innerText.trim(),
+                                    link: baseUrl + link,
+                                    author: author.innerText.trim(),
+                                    hit,
+                                    time: time,
+                                    upVotes: parseInt(upVotes),
+                                    downVotes: parseInt(downVotes),
+                                    numberOfComments: numberOfComments
+                                        ? parseInt(numberOfComments.innerText)
+                                        : 0,
+                                }
+                            );
+                        })
+                        .filter((post) => post);
+                } else {
+                    const bbs = document.querySelector('.bbsList_new');
+                    const lists2 = bbs.querySelectorAll('li a');
+
+                    return Array.from(lists2).map((list) => {
+                        const category = list.querySelector('.exp span.ty');
+                        const title = list.querySelector('.title .cont');
+                        const author = list.querySelector('.names');
+                        const time = list.querySelector('time');
+                        const numberOfComments = list.querySelector('.title .rp');
+
+                        const infoEl = list
+                            .querySelector('.exp span:last-child')
+                            .innerText.split('/')
+                            .map((el) => el.replace(/[^0-9]/g, ''));
+
+                        const hit = infoEl[0];
+                        const upVotes = infoEl[1];
+                        const downVotes = infoEl[2];
 
                         let link = list.getAttribute('href');
 
-                        if (link && !link.includes('/new/')) {
-                            if (link.includes('ppomppu2')) {
-                                link = '/new/bbs_view.php' + link;
-                            } else {
-                                link = '/new/' + link;
-                            }
+                        if (!link.startsWith('/new/')) {
+                            link = '/new/' + link;
                         }
 
-                        return (
-                            author &&
-                            author.innerText && {
-                                category,
-                                title: title.innerText.trim(),
-                                link: baseUrl + link,
-                                author: author.innerText.trim(),
-                                hit,
-                                time: time,
-                                upVotes: parseInt(upVotes),
-                                downVotes: parseInt(downVotes),
-                                numberOfComments: numberOfComments
-                                    ? parseInt(numberOfComments.innerText)
-                                    : 0,
-                            }
-                        );
-                    })
-                    .filter((post) => post);
-            } else {
-                const bbs = document.querySelector('.bbsList_new');
-                const lists2 = bbs.querySelectorAll('li a');
-
-                return Array.from(lists2).map((list) => {
-                    const category = list.querySelector('.exp span.ty');
-                    const title = list.querySelector('.title .cont');
-                    const author = list.querySelector('.names');
-                    const time = list.querySelector('time');
-                    const numberOfComments = list.querySelector('.title .rp');
-
-                    const infoEl = list
-                        .querySelector('.exp span:last-child')
-                        .innerText.split('/')
-                        .map((el) => el.replace(/[^0-9]/g, ''));
-
-                    const hit = infoEl[0];
-                    const upVotes = infoEl[1];
-                    const downVotes = infoEl[2];
-
-                    let link = list.getAttribute('href');
-
-                    if (!link.startsWith('/new/')) {
-                        link = '/new/' + link;
-                    }
-
-                    return {
-                        category: category.innerText,
-                        title: title.innerText.trim(),
-                        link: baseUrl + link,
-                        time: time.innerText,
-                        author: author.innerText,
-                        hit,
-                        upVotes: parseInt(upVotes),
-                        downVotes: parseInt(downVotes),
-                        numberOfComments: numberOfComments
-                            ? parseInt(numberOfComments.innerText)
-                            : 0,
-                    };
-                });
-            }
-        }, baseUrl);
+                        return {
+                            category: category.innerText,
+                            title: title.innerText.trim(),
+                            link: baseUrl + link,
+                            time: time.innerText,
+                            author: author.innerText,
+                            hit,
+                            upVotes: parseInt(upVotes),
+                            downVotes: parseInt(downVotes),
+                            numberOfComments: numberOfComments
+                                ? parseInt(numberOfComments.innerText)
+                                : 0,
+                        };
+                    });
+                }
+            },
+            baseUrl,
+            this.currentBoard.value
+        );
 
         return posts.map((post) => ({ ...post, hasRead: this.postsRead.has(post.link) }));
     }
