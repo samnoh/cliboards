88fc955b5ae6acee80c93a4f4d1200a71b6b63f2
Sam Noh <samnoh21@outlook.com>
2020-06-15 16:45:26 +1200

    .

--- src/crawler/Crawler.js
+++ src/crawler/Crawler.js
@@ -16,7 +16,7 @@ class Crawler {
 
         try {
             this.browser = await puppeteer.launch({
-                headless: false,
+                headless: true,
                 args: [
                     '--no-sandbox',
                     '--disable-setuid-sandbox',
