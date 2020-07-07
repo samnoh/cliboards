aff4c6c2514e6d35e91a4a7efbe62b0120235da1
Sam Noh <samnoh21@outlook.com>
2020-07-07 19:20:53 +1200

    clean up

--- test/crawler/clien.test.js
+++ test/crawler/clien.test.js
@@ -11,81 +11,83 @@ afterAll(async () => {
     await clien.close();
 });
 
-describe('Clien class properties', () => {
-    test('title', () => {
-        expect(clien.title).toEqual('Clien');
+describe('Clien', () => {
+    describe('properties', () => {
+        test('title', () => {
+            expect(clien.title).toEqual('Clien');
+        });
+
+        test('boardTypes', () => {
+            expect(clien.boardTypes).toEqual(['커뮤니티', '소모임']);
+        });
+
+        test('sortUrls', () => {
+            expect(clien.sortUrls).toEqual([
+                {
+                    name: '등록일순',
+                    value: '&od=T31',
+                },
+                {
+                    name: '공감순',
+                    value: '&od=T33',
+                },
+                {
+                    name: '댓글순',
+                    value: '&od=T34',
+                },
+            ]);
+        });
+
+        test('baseUrl', () => {
+            expect(clien.baseUrl).toEqual('https://www.clien.net');
+        });
     });
 
-    test('boardTypes', () => {
-        expect(clien.boardTypes).toEqual(['커뮤니티', '소모임']);
-    });
+    describe('methods', () => {
+        let posts;
 
-    test('sortUrls', () => {
-        expect(clien.sortUrls).toEqual([
-            {
-                name: '등록일순',
-                value: '&od=T31',
-            },
-            {
-                name: '공감순',
-                value: '&od=T33',
-            },
-            {
-                name: '댓글순',
-                value: '&od=T34',
-            },
-        ]);
-    });
+        test('getBoards()', async () => {
+            await clien.getBoards();
 
-    test('baseUrl', () => {
-        expect(clien.baseUrl).toEqual('https://www.clien.net');
-    });
-});
+            expect(
+                clien.boards.filter(b => b.type === '커뮤니티').length,
+            ).toEqual(11);
+        });
 
-describe('Clien class methods', () => {
-    let posts;
+        test('getPosts()', async () => {
+            posts = await clien.changeBoard(clien.boards[0]);
 
-    test('getBoards()', async () => {
-        await clien.getBoards();
+            expect(posts.length).not.toBe(0);
+        });
 
-        expect(clien.boards.filter(b => b.type === '커뮤니티').length).toEqual(
-            11,
-        );
-    });
+        test('getPostDetail()', async () => {
+            const post = await clien.getPostDetail(posts[0]);
 
-    test('getPosts()', async () => {
-        posts = await clien.changeBoard(clien.boards[0]);
+            expect(post.category).not.toBeUndefined();
+            expect(post.author).toBeTruthy();
+            expect(post.title).toBeTruthy();
+            expect(post.body).toBeTruthy();
+            expect(post.hit).toBeTruthy();
+            expect(post.time).toBeTruthy();
+            expect(Array.isArray(post.comments)).toBeTruthy();
+        });
 
-        expect(posts.length).not.toBe(0);
-    });
+        test('processComments()', async () => {
+            const postWithComments = posts.find(p => p.numberOfComments);
+            const post = await clien.getPostDetail(postWithComments);
+            const comment = post.comments[0];
 
-    test('getPostDetail()', async () => {
-        const post = await clien.getPostDetail(posts[0]);
+            expect(comment.id).toBeTruthy();
+            expect(comment.author).toBeTruthy();
+            expect(comment.body).toBeTruthy();
+            expect(comment.time).toBeTruthy();
+        });
 
-        expect(post.category).not.toBeUndefined();
-        expect(post.author).toBeTruthy();
-        expect(post.title).toBeTruthy();
-        expect(post.body).toBeTruthy();
-        expect(post.hit).toBeTruthy();
-        expect(post.time).toBeTruthy();
-        expect(Array.isArray(post.comments)).toBeTruthy();
-    });
+        test('getAllComments()', async () => {
+            const post = await clien.getPostDetail(posts[0]);
+            const allComments = await clien.getAllComments();
 
-    test('processComments()', async () => {
-        const postWithComments = posts.find(p => p.numberOfComments);
-        const post = await clien.getPostDetail(postWithComments);
-        const comment = post.comments[0];
-
-        expect(comment.id).toBeTruthy();
-        expect(comment.author).toBeTruthy();
-        expect(comment.body).toBeTruthy();
-        expect(comment.time).toBeTruthy();
-    });
-
-    test('getAllComments()', async () => {
-        const post = await clien.getPostDetail(posts[0]);
-        const allComments = await clien.getAllComments();
-
-        expect(allComments.length).toEqual(post.comments.length);
+            expect(allComments.length).toEqual(post.comments.length);
+        });
     });
 });
--- test/crawler/dcinside.test.js
+++ test/crawler/dcinside.test.js
@@ -13,102 +13,104 @@ afterAll(async () => {
     await dcinside.close();
 });
 
-describe('Dcinside class properties', () => {
-    test('title', () => {
-        expect(dcinside.title).toEqual('dcinside');
-    });
+describe('Dcinside', () => {
+    describe('properties', () => {
+        test('title', () => {
+            expect(dcinside.title).toEqual('dcinside');
+        });
 
-    test('boardTypes', () => {
-        expect(dcinside.boardTypes).toEqual([
-            '1 페이지',
-            '2 페이지',
-            '3 페이지',
-        ]);
-    });
+        test('boardTypes', () => {
+            expect(dcinside.boardTypes).toEqual([
+                '1 페이지',
+                '2 페이지',
+                '3 페이지',
+            ]);
+        });
 
-    test('searchTypes', () => {
-        expect(dcinside.searchTypes).toEqual([
-            {
-                name: '전체',
-                value: 'all',
-            },
-            {
-                name: '제목',
-                value: 'subject',
-            },
-            {
-                name: '내용',
-                value: 'memo',
-            },
-            {
-                name: '글쓴이',
-                value: 'name',
-            },
-            {
-                name: '제목+내용',
-                value: 'subject_m',
-            },
-        ]);
-    });
-
-    test('filterOptions', () => {
-        expect(dcinside.filterOptions).toEqual({
-            activeFilterIndex: 0,
-            filters: [
+        test('searchTypes', () => {
+            expect(dcinside.searchTypes).toEqual([
                 {
                     name: '전체',
-                    value: '',
+                    value: 'all',
                 },
                 {
-                    name: '개념글',
-                    value: 'recommend=1',
+                    name: '제목',
+                    value: 'subject',
                 },
-            ],
+                {
+                    name: '내용',
+                    value: 'memo',
+                },
+                {
+                    name: '글쓴이',
+                    value: 'name',
+                },
+                {
+                    name: '제목+내용',
+                    value: 'subject_m',
+                },
+            ]);
+        });
+
+        test('filterOptions', () => {
+            expect(dcinside.filterOptions).toEqual({
+                activeFilterIndex: 0,
+                filters: [
+                    {
+                        name: '전체',
+                        value: '',
+                    },
+                    {
+                        name: '개념글',
+                        value: 'recommend=1',
+                    },
+                ],
+            });
         });
     });
-});
 
-describe('Dcinside class methods', () => {
-    let posts;
+    describe('methods', () => {
+        let posts;
 
-    test('getBoards()', async () => {
-        await dcinside.getBoards();
-        expect(
-            dcinside.boards.filter(b => b.type === dcinside.boardTypes[0])
-                .length,
-        ).toEqual(4);
-    });
+        test('getBoards()', async () => {
+            await dcinside.getBoards();
+            expect(
+                dcinside.boards.filter(b => b.type === dcinside.boardTypes[0])
+                    .length,
+            ).toEqual(4);
+        });
 
-    test('getPosts()', async () => {
-        posts = await dcinside.changeBoard(dcinside.boards[0]);
+        test('getPosts()', async () => {
+            posts = await dcinside.changeBoard(dcinside.boards[0]);
 
-        expect(posts.length).not.toBe(0);
-    });
+            expect(posts.length).not.toBe(0);
+        });
 
-    test('getPostDetail()', async () => {
-        const post = await dcinside.getPostDetail(posts[0]);
+        test('getPostDetail()', async () => {
+            const post = await dcinside.getPostDetail(posts[0]);
 
-        expect(post.author).toBeTruthy();
-        expect(post.title).toBeTruthy();
-        expect(post.body).toBeTruthy();
-        expect(post.hit).toBeTruthy();
-        expect(post.time).toBeTruthy();
-        expect(Array.isArray(post.comments)).toBeTruthy();
-    });
+            expect(post.author).toBeTruthy();
+            expect(post.title).toBeTruthy();
+            expect(post.body).toBeTruthy();
+            expect(post.hit).toBeTruthy();
+            expect(post.time).toBeTruthy();
+            expect(Array.isArray(post.comments)).toBeTruthy();
+        });
 
-    test('addBoard()', async () => {
-        await dcinside.addBoard('cat', dcinside.boardTypes[0]);
-        await dcinside.getBoards();
-        await dcinside.addBoard('dog', dcinside.boardTypes[1]);
-        await dcinside.getBoards();
+        test('addBoard()', async () => {
+            await dcinside.addBoard('cat', dcinside.boardTypes[0]);
+            await dcinside.getBoards();
+            await dcinside.addBoard('dog', dcinside.boardTypes[1]);
+            await dcinside.getBoards();
 
-        expect(
-            dcinside.boards.filter(b => b.type === dcinside.boardTypes[0])
-                .length,
-        ).toEqual(5);
-        expect(
-            dcinside.boards.filter(b => b.type === dcinside.boardTypes[1])
-                .length,
-        ).toEqual(1);
+            expect(
+                dcinside.boards.filter(b => b.type === dcinside.boardTypes[0])
+                    .length,
+            ).toEqual(5);
+            expect(
+                dcinside.boards.filter(b => b.type === dcinside.boardTypes[1])
+                    .length,
+            ).toEqual(1);
+        });
     });
 });
