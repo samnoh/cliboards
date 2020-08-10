ce78b07a014ba041a38309e6ede60a758479b181
Sam Noh <samnoh21@outlook.com>
2020-08-10 16:18:53 +1200

    update static js

--- src/static/js/autoClose.js
+++ src/static/js/autoClose.js
@@ -1,7 +1,12 @@
 'use strict';
 
 (() => {
+    const isAutoClose = localStorage.getItem('isAutoClose');
     const autoCloseInput = document.querySelector('#auto-close');
+
+    if (!isAutoClose === 'true' && !isAutoClose === null) {
+        autoCloseInput.checked = false;
+    }
 
     let isReloaded = false;
 
@@ -22,6 +27,7 @@
     }
 
     window.addEventListener('beforeunload', function (e) {
+        localStorage.setItem('isAutoClose', autoCloseInput.checked);
         history.scrollRestoration = 'manual';
         isReloaded = true;
     });
